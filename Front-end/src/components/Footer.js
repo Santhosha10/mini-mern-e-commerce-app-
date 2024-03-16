78a39f3c95adc0275681acd3e732912d364e27f3
SANTHOSH <127328996+Santhosha10@users.noreply.github.com>
2024-03-16 20:31:08 +0530

    published

--- Front-end/src/components/Footer.js
+++ Front-end/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return  <footer className="py-1 bg-dark">
   <p className="text-center text-white mt-1 ">
-      JVLcart - 2023-2024, All Rights Reserved
+      SHOP @2024, All Rights Reserved
   </p>
   </footer>
 }
